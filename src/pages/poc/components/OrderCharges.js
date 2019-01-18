93c27fc0481f4b4fc5d4fe5cfd583bf4241caf4b
QitieLi <liqitiesir@gmail.com>
2019-01-18 10:08:31 +0800

    integrate with extjs

--- src/pages/poc/components/OrderCharges.js
+++ src/pages/poc/components/OrderCharges.js
@@ -1,40 +1,53 @@
 import React from 'react';
-import { Table } from 'antd';
 
-export default props => {
-  const columns = [
-    {
-      title: 'Code',
-      dataIndex: 'lineSourceName',
-    },
-    {
-      title: 'Draft Invoice Amount',
-      dataIndex: 'inInvoicingAmount',
-    },
-    {
-      title: 'Description(EN)',
-      dataIndex: 'descriptionEn',
-    },
-    {
-      title: 'Charge',
-      dataIndex: 'amount',
-      render: value => `$${value}`,
-    },
-    {
-      title: 'Invoiced Amount',
-      dataIndex: 'invoicedAmount',
-    },
-    {
-      title: 'Tax Amount',
-      dataIndex: 'taxAmount',
-    },
-    {
-      title: 'Description(FR)',
-      dataIndex: 'descriptionFr',
-    },
-  ];
+class OrderCharges extends React.Component {
 
-  const { data, loading } = props;
+  componentDidMount() {
+    Ext.create('Ext.data.Store', {
+      storeId: 'simpsonsStore',
+      fields:[ 'name', 'email', 'phone'],
+      data: [
+        { name: 'Lisa', email: 'lisa@simpsons.com', phone: '555-111-1224' },
+        { name: 'Bart', email: 'bart@simpsons.com', phone: '555-222-1234' },
+        { name: 'Homer', email: 'homer@simpsons.com', phone: '555-222-1244' },
+        { name: 'Marge', email: 'marge@simpsons.com', phone: '555-222-1254' }
+      ]
+    });
 
-  return <Table rowKey="id" columns={columns} dataSource={data} />;
-};
+    Ext.create('Ext.grid.Panel', {
+      title: 'Simpsons',
+      store: Ext.data.StoreManager.lookup('simpsonsStore'),
+      columns: [
+        {header: 'Name', dataIndex: 'name', editor: 'textfield'},
+        {header: 'Email', dataIndex: 'email', flex:1,
+          editor: {
+            completeOnEnter: false,
+
+            // If the editor config contains a field property, then
+            // the editor config is used to create the Ext.grid.CellEditor
+            // and the field property is used to create the editing input field.
+            field: {
+              xtype: 'textfield',
+              allowBlank: false
+            }
+          }
+        },
+        {header: 'Phone', dataIndex: 'phone'}
+      ],
+      selModel: 'cellmodel',
+      plugins: {
+        ptype: 'cellediting',
+        clicksToEdit: 1
+      },
+      // height: 200,
+      // width: 400,
+      renderTo: this.el
+    });
+  }
+
+  render() {
+    return <div ref={el => this.el = el} />
+  }
+}
+
+export default OrderCharges
